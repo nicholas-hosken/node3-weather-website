17117210878805b92ff25131d90dddf1e4bed35a
Nicholas Hosken <nicholas.hosken@yahoo.com>
2024-01-08 13:24:21 -0500

    Added UV Index to output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, location, callback) => {
 		} else if (body.error) {
 			callback(`Unable to find location.`, undefined);
 		} else {
-			callback(undefined, `${body.current.weather_descriptions[0]}. The temperature in ${location} is currently ${body.current.temperature}C out and it feels like ${body.current.feelslike}C.`);
+			callback(undefined, `${body.current.weather_descriptions[0]}. The temperature in ${location} is currently ${body.current.temperature}C out and it feels like ${body.current.feelslike}C. The currect UV index is:${body.current.uv_index}`);
 		}
 	});
 };
